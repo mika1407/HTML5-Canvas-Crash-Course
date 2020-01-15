891b362a35f660f97053560294283826104c2823
mika1407 <koneautomaatio@hotmail.com>
2020-01-15 11:26:56 +0200

    ball animation

--- script.js
+++ script.js
@@ -53,39 +53,80 @@ const ctx = canvas.getContext("2d");
 // ctx.fill();
 
 // Arc (circles)
-ctx.beginPath();
+// ctx.beginPath();
 
-const centerX = canvas.width / 2;
-const centerY = canvas.height / 2;
+// const centerX = canvas.width / 2;
+// const centerY = canvas.height / 2;
 
-// Draw head
-ctx.arc(centerX, centerY, 200, 0, Math.PI * 2);
+// // Draw head
+// ctx.arc(centerX, centerY, 200, 0, Math.PI * 2);
 
-// Move to mouth
-ctx.moveTo(centerX + 100, centerY);
+// // Move to mouth
+// ctx.moveTo(centerX + 100, centerY);
 
-// Draw mouth
-ctx.arc(centerX, centerY, 100, 0, Math.PI, false);
+// // Draw mouth
+// ctx.arc(centerX, centerY, 100, 0, Math.PI, false);
 
-// Move left eye
-ctx.moveTo(centerX - 60, centerY - 80);
+// // Move left eye
+// ctx.moveTo(centerX - 60, centerY - 80);
 
-// Draw left eye
-ctx.arc(centerX - 80, centerY - 80, 20, 0, Math.PI * 2);
+// // Draw left eye
+// ctx.arc(centerX - 80, centerY - 80, 20, 0, Math.PI * 2);
 
-// Move right eye
-ctx.moveTo(centerX + 100, centerY - 80);
+// // Move right eye
+// ctx.moveTo(centerX + 100, centerY - 80);
 
-// Draw right eye
-ctx.arc(centerX + 80, centerY - 80, 20, 0, Math.PI * 2);
+// // Draw right eye
+// ctx.arc(centerX + 80, centerY - 80, 20, 0, Math.PI * 2);
 
-// Quadratric curves example
-ctx.moveTo(75, 25);
-ctx.quadraticCurveTo(25, 25, 25, 62.5);
-ctx.quadraticCurveTo(25, 100, 50, 100);
-ctx.quadraticCurveTo(50, 120, 30, 125);
-ctx.quadraticCurveTo(60, 120, 65, 100);
-ctx.quadraticCurveTo(125, 100, 125, 62.5);
-ctx.quadraticCurveTo(125, 25, 75, 25);
+// // Quadratric curves example
+// ctx.moveTo(75, 25);
+// ctx.quadraticCurveTo(25, 25, 25, 62.5);
+// ctx.quadraticCurveTo(25, 100, 50, 100);
+// ctx.quadraticCurveTo(50, 120, 30, 125);
+// ctx.quadraticCurveTo(60, 120, 65, 100);
+// ctx.quadraticCurveTo(125, 100, 125, 62.5);
+// ctx.quadraticCurveTo(125, 25, 75, 25);
 
-ctx.stroke();
+// ctx.stroke();
+
+// Animation 1
+
+const circle = {
+  x: 200,
+  y: 200,
+  size: 30,
+  dx: 5,
+  dy: 4
+};
+
+function drawCircle() {
+  ctx.beginPath();
+  ctx.arc(circle.x, circle.y, circle.size, 0, Math.PI * 2);
+  ctx.fillStyle = "purple";
+  ctx.fill();
+}
+
+function update() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  drawCircle();
+
+  // change position
+  circle.x += circle.dx;
+  circle.y += circle.dy;
+
+  // Detect side walls
+  if (circle.x + circle.size > canvas.width || circle.x - circle.size < 0) {
+    circle.dx *= -1; // circle.dx = circle.dx * -1
+  }
+
+  // Detect top and bottom walls
+  if (circle.y + circle.size > canvas.height || circle.y - circle.size < 0) {
+    circle.dy *= -1;
+  }
+
+  requestAnimationFrame(update);
+}
+
+update();
